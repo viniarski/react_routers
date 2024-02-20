eb2ec103fe9d9242ba2d11037854836f33525544
Michal Winiarski <41969647+viniarski@users.noreply.github.com>
2024-02-20 14:11:38 +0000

    gallery change

--- src/stores/gallery.js
+++ src/stores/gallery.js
@@ -2,31 +2,37 @@ export const gallery = [
   {
     carName: 'Dodge Charger',
     engine: 'V8',
-    image: '../src/assets/images/1.avif',
+    image:
+      'https://unsplash.com/photos/orange-chevrolet-camaro-on-dirt-road-during-daytime-GUFR1WsgCQQ',
   },
   {
-    carName: 'New Dodge Charger',
+    carName: 'Dodge Charger',
     engine: 'V8',
-    image: '../src/assets/images/2.avif',
-  },
-  {
-    carName: 'Dodge Charger R/T',
-    engine: 'V12',
-    image: '../src/assets/images/3.avif',
-  },
-  {
-    carName: 'Chevy Camaro',
-    engine: 'V8',
-    image: '../src/assets/images/4.avif',
-  },
-  {
-    carName: 'Dodge Challenger',
-    engine: 'V12',
-    image: '../src/assets/images/5.avif',
+    image:
+      'https://unsplash.com/photos/green-chevrolet-camaro-on-gray-asphalt-road-during-daytime-qdIUrufLBg4',
   },
   {
     carName: 'Ford Mustang',
     engine: 'V8',
-    image: '../src/assets/images/6.avif',
+    image:
+      'https://unsplash.com/photos/grayscale-photo-of-mercedes-benz-coupe-HsB4t2NvtLo',
+  },
+  {
+    carName: 'Chevy Camaro',
+    engine: 'V8',
+    image:
+      'https://unsplash.com/photos/green-chevrolet-camaro-parked-on-parking-lot-during-daytime-xe3QnxPgnHA',
+  },
+  {
+    carName: 'Dodge Challenger',
+    engine: 'V12',
+    image:
+      'https://unsplash.com/photos/black-coupe-on-concrete-road-799KfBloSFQ',
+  },
+  {
+    carName: 'Ford Mustang',
+    engine: 'V8',
+    image:
+      'https://unsplash.com/photos/red-chevrolet-camaro-on-road-during-daytime-BwBxVVdlpYE',
   },
 ];
