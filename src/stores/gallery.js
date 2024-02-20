dc0793daaecf7c15c9ec8dabfa3e978b3e1217f9
Michal Winiarski <41969647+viniarski@users.noreply.github.com>
2024-02-20 14:47:44 +0000

    gallery links update

--- src/stores/gallery.js
+++ src/stores/gallery.js
@@ -27,7 +27,7 @@ export const gallery = [
     carName: 'Dodge Challenger',
     engine: 'V12',
     image:
-      'https://images.unsplash.com/photo-1626780571746-da9a96c2896c?q=80&whttps://unsplash.com/photos/black-coupe-on-concrete-road-799KfBloSFQ=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      'https://images.unsplash.com/photo-1495582811045-39f82b39c33b?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   },
   {
     carName: 'Ford Mustang',
